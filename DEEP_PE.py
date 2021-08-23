097ff496874f4ea369222e177e6f66a1f9295912
Keval Raval <Keval@Kevals-MacBook-Pro.local>
2021-08-23 00:08:00 -0400

    Made GUI, .gitignore, some functionality

---
+++ DEEP_PE.py
@@ -0,0 +1,72 @@
+from tkinter import *
+from tkinter import filedialog
+import regex as re
+
+window = Tk()
+window.title("DEEP-PE Searching Tool")
+
+def selectFile():
+    global filename
+    filename = filedialog.askopenfilename(initialdir='/',title='Select File',
+    filetypes=(('Text File','*.txt'),('All Files', '*.*')))
+    if(filename!=''):
+        label = Label(frame, text=filename, bg = 'gray')
+        spaceText.window_create("end", window=label)
+        spaceText.insert('end', '\n')
+
+def main():
+    file = open("DeepPE_Scores.txt", "r")
+    content = file.read()
+
+    contentList = content.splitlines()
+    del contentList[0]
+    print (contentList[0])
+    parsedList = []
+
+    for line in contentList:
+        tempSplit = line.split("\t")
+        parsedList.append(tempSplit)
+
+    print (parsedList[0])
+
+    for line in parsedList:
+        changeList = line[4].split(" ")
+        originalBase = changeList[3]
+        newBase = changeList[1].lower()
+        RTTLength = int(line[5])
+        extension = line[7]
+        count = 0
+        searchSeq = "".join(extension[0:RTTLength])
+        tempList = list(searchSeq)
+        for count, base in enumerate(tempList):
+            if base.islower():
+                tempList[count] = originalBase
+        searchSeq = "".join(tempList)
+        result = re.search(searchSeq, inputFASTA, re.IGNORECASE)
+        print (result)
+
+canvas = Canvas(window, height = 100, width = 600)
+canvas.pack()
+
+frame = Frame(window,relief = 'groove')
+frame.place(relx = 0.1, rely = 0.1, relwidth = 0.8, relheight = 0.8)
+
+welcome = Label(frame, text = "Welcome to the DEEP-PE Searching and Analysis Tool", fg = "Black")
+welcome.pack(side = "top")
+
+inputFASTA = Entry(frame, width = 50)
+inputFASTA.pack(side = "top")
+inputFASTA.insert(0, "Please enter the inputted FASTA sequence")
+
+spaceText = Text(frame,width=40,height=20, borderwidth=0)
+spaceText.pack(side='right',fill='both' ,expand=True)
+
+openFile = Button(window, text='Open DEEP-PE results .txt File', padx = 10, pady = 5, fg = 'black', bg = 'gray', command = selectFile)
+openFile.pack()
+
+enterButton = Button(window, text = "Start", padx = 10, pady = 5, fg = "Black", bg = "gray", command = main)
+enterButton.pack()
+
+window.mainloop()
+
+
